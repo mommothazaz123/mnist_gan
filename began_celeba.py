1f906b4300b4175c93d36a6db43135d5925fea29
Andrew Zhu <mommothazaz123@gmail.com>
2019-05-03 12:59:51 -0700

    fix began, add samples

--- began_celeba.py
+++ began_celeba.py
@@ -15,19 +15,19 @@ class CelebABEGAN:
         self.z = z
         self.n = n
 
-        optimizer = tf.keras.optimizers.Adam(0.00005)
+        # for training
+        self.k = 0
+
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='mae', optimizer=optimizer)
 
         # Build and compile the generator
         self.generator = self.build_generator()
-        self.generator.compile(loss='mae', optimizer=optimizer)
 
         # Build and compile the combined model
         self.combined = self.build_combined()
-        self.combined.compile(loss='mae', optimizer=optimizer)
 
     def load(self, path):
         path = path.rstrip('/')
@@ -60,14 +60,15 @@ class CelebABEGAN:
 
     def decoder(self, h):
         embedding_shape = (h,)
-        noise = tf.keras.layers.Input(shape=embedding_shape, name="h")
+        noise = tf.keras.layers.Input(shape=embedding_shape, name="h", dtype="float32")
 
-        hid = tf.keras.layers.Dense(8 * 8 * self.n, activation='relu', input_shape=embedding_shape)(noise)
+        hid = tf.keras.layers.Dense(8 * 8 * self.n, input_shape=embedding_shape, activation='elu')(noise)
         hid = tf.keras.layers.Reshape((8, 8, self.n))(hid)
 
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
+        hid = tf.keras.layers.BatchNormalization(momentum=0.8)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         # (None, 8, 8, n)
 
@@ -75,6 +76,7 @@ class CelebABEGAN:
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
+        hid = tf.keras.layers.BatchNormalization(momentum=0.8)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         # (None, 16, 16, n)
 
@@ -82,6 +84,7 @@ class CelebABEGAN:
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
+        hid = tf.keras.layers.BatchNormalization(momentum=0.8)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         # (None, 32, 32, n)
 
@@ -89,20 +92,23 @@ class CelebABEGAN:
         # (None, 32, 32, 3)
 
         model = tf.keras.models.Model(inputs=noise, outputs=img)
+        model.summary()
         return model
 
     def encoder(self):
-        img = tf.keras.layers.Input(shape=self.img_shape, name="image")
+        img = tf.keras.layers.Input(shape=self.img_shape, name="image", dtype="float32")
 
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(img)
         hid = tf.keras.layers.Activation('elu')(hid)
         hid = tf.keras.layers.Conv2D(self.n, kernel_size=3, padding='same')(hid)
+        hid = tf.keras.layers.BatchNormalization(momentum=0.8)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         # (None, 32, 32, n)
 
         hid = tf.keras.layers.Conv2D(2 * self.n, kernel_size=3, padding='same', strides=2)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         hid = tf.keras.layers.Conv2D(2 * self.n, kernel_size=3, padding='same')(hid)
+        hid = tf.keras.layers.BatchNormalization(momentum=0.8)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         # (None, 16, 16, 2n)
 
@@ -111,13 +117,15 @@ class CelebABEGAN:
         hid = tf.keras.layers.Conv2D(3 * self.n, kernel_size=3, padding='same')(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         hid = tf.keras.layers.Conv2D(3 * self.n, kernel_size=3, padding='same')(hid)
+        hid = tf.keras.layers.BatchNormalization(momentum=0.8)(hid)
         hid = tf.keras.layers.Activation('elu')(hid)
         # (None, 8, 8, 3n)
 
         hid = tf.keras.layers.Flatten()(hid)
-        enc_img = tf.keras.layers.Dense(self.h, activation='sigmoid')(hid)
+        enc_img = tf.keras.layers.Dense(self.h)(hid)
 
         model = tf.keras.models.Model(inputs=img, outputs=enc_img)
+        model.summary()
         return model
 
     def autoencoder(self):
@@ -130,28 +138,38 @@ class CelebABEGAN:
 
     def build_generator(self):
         m = self.decoder(self.z)
-        m.summary()
         return m
 
     def build_discriminator(self):
         m = self.autoencoder()
-        m.summary()
         return m
 
     def build_combined(self):
         # discrim should be compiled before calling this
         z = tf.keras.layers.Input(shape=(self.z,), name="noise")
         img = self.generator(z)
-        self.discriminator.trainable = False
         valid = self.discriminator(img)
         return tf.keras.models.Model(inputs=z, outputs=valid)
+
+    def l1loss(self, model, x, y):
+        y_ = model(x)
+        return tf.reduce_mean(tf.abs(y_ - y))
+
+    def grad(self, model, inputs, targets):
+        with tf.GradientTape() as tape:
+            loss_value = self.l1loss(model, inputs, targets)
+        return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+    def d_grad(self, model, real_in, real_out, gen_in, gen_out):
+        with tf.GradientTape() as tape:
+            real_loss = self.l1loss(model, real_in, real_out)
+            gen_loss = -self.k * self.l1loss(model, gen_in, gen_out)
+            loss_value = real_loss + gen_loss
+        return real_loss, gen_loss, loss_value, tape.gradient(loss_value, model.trainable_variables)
 
     def train(self, x, epochs, k_lambda, gamma, batch_size=16, sample_interval=50, sample_path="samples/unknown",
               starting_epoch=0, save_interval=5000):
         ensure_exists(sample_path)
-
-        # set up hyperparameters
-        k = tf.keras.backend.epsilon()
 
         epoch = starting_epoch
         while epoch < epochs:
@@ -165,32 +183,42 @@ class CelebABEGAN:
 
             # Generate a batch of new images
             noise = np.random.uniform(-1, 1, (batch_size, self.z))
-            fake = self.generator.predict(noise)
-
-            weights = -k * np.ones(batch_size)
+            noise = noise.astype("float32")
+            fake = self.generator(noise)
 
             # train
-            d_loss_real = self.discriminator.train_on_batch(real, real)
-            d_loss_fake = self.discriminator.train_on_batch(fake, fake, weights)
-            d_loss = d_loss_real + d_loss_fake
+            d_loss_real, d_loss_gen, d_loss, d_grads = self.d_grad(self.discriminator, real, real, fake, fake)
+            # print(d_loss_real)
+            # print(d_loss_fake)
+            self.optimizer.apply_gradients(zip(d_grads, self.discriminator.trainable_variables))
 
             # ---------------------
             #  Train Generator
             # ---------------------
             # Generate a batch of new images
             noise = np.random.uniform(-1, 1, (2 * batch_size, self.z))
-            target = self.generator.predict(noise)
+            noise = noise.astype("float32")
+            fake = self.generator(noise)
 
             # Train the generator on generated images?
-            g_loss = self.combined.train_on_batch(noise, target)
+            self.discriminator.trainable = False
+            g_loss, g_grads = self.grad(self.combined, noise, fake)
+            self.optimizer.apply_gradients(zip(g_grads, self.combined.trainable_variables))
+            self.discriminator.trainable = True
 
             # ---------------------
             #  Update k
             # ---------------------
             # maintains the diversity ratio of the network - balances goals of
             # autoencoding and discriminating images
-            k = k + k_lambda * (gamma * d_loss_real - g_loss)
-            k = min(max(k, tf.keras.backend.epsilon()), 1)
+            self.k = self.k + k_lambda * (gamma * d_loss_real - g_loss)
+            self.k = min(max(self.k, 0), 1)
+
+            # ---------------------
+            #  LR Decay
+            # ---------------------
+            if epoch % 30000 == 0:
+                self.optimizer = tf.train.AdamOptimizer(learning_rate=0.0001 * pow(0.5, epoch // 30000))
 
             # ---------------------
             #  Status report
@@ -198,7 +226,9 @@ class CelebABEGAN:
             # calculate convergence factor
             m_global = d_loss + np.abs(gamma * d_loss_real - g_loss)
             # Plot the progress
-            print("%d [M: %f] [D loss: %f] [G loss: %f] [K: %f]" % (epoch, m_global, d_loss, g_loss, k))
+            print("%d [M: %f] [D loss: %f = %f + %f] [G loss: %f] [K: %f]" % (
+                epoch, m_global, d_loss.numpy(), d_loss_real.numpy(), d_loss_gen.numpy(), g_loss.numpy(),
+                self.k))
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
@@ -219,38 +249,34 @@ class CelebABEGAN:
         noise = np.random.uniform(-1, 1, (r * c, self.z))
         gen_imgs = self.generator.predict(noise)
 
-        # Rescale images 0 - 1
-        gen_imgs = 0.5 * gen_imgs + 0.5
-        real_imgs = 0.5 * real_imgs + 0.5
+        combined_images = self.combined.predict(noise)
 
-        fig, axs = plt.subplots(r, c, figsize=(6, 5))
-        fig.subplots_adjust(left=0.03, right=0.97, hspace=0.3, wspace=0.05)
-        cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i, j].imshow(gen_imgs[cnt, :, :, :])
-                axs[i, j].axis('off')
-                cnt += 1
-        fig.savefig(f"{path}/g-{epoch}.png", dpi=100)
-        plt.close()
+        def save(imgs, fpath):
+            # Rescale images 0 - 1
+            imgs = 0.5 * imgs + 0.5
 
-        fig, axs = plt.subplots(r, c, figsize=(6, 5))
-        fig.subplots_adjust(left=0.03, right=0.97, hspace=0.3, wspace=0.05)
-        cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i, j].imshow(real_imgs[cnt, :, :, :])
-                axs[i, j].axis('off')
-                cnt += 1
-        fig.savefig(f"{path}/d-{epoch}.png", dpi=100)
-        plt.close()
+            fig, axs = plt.subplots(r, c, figsize=(self.img_cols / 10, self.img_rows / 12))
+            fig.subplots_adjust(left=0.03, right=0.97, hspace=0.3, wspace=0.05)
+            cnt = 0
+            for i in range(r):
+                for j in range(c):
+                    axs[i, j].imshow(imgs[cnt, :, :, :])
+                    axs[i, j].axis('off')
+                    cnt += 1
+            fig.savefig(fpath, dpi=100)
+            plt.close()
+
+        save(gen_imgs, f"{path}/g-{epoch}.png")
+        save(real_imgs, f"{path}/d-{epoch}.png")
+        save(combined_images, f"{path}/c-{epoch}.png")
 
 
 if __name__ == '__main__':
-    gan = CelebABEGAN(img_rows=32, img_cols=32, img_channels=3, h=64, z=100, n=64)
+    tf.enable_eager_execution()
+    gan = CelebABEGAN(img_rows=32, img_cols=32, img_channels=3, h=64, z=64, n=64)
 
-    x = celeba_32()
+    x = celeba_32(260000)
 
-    gan.train(x, epochs=200001, k_lambda=0.001, gamma=0.5, batch_size=16, sample_interval=200,
-              sample_path="samples/celeba_began_32", save_interval=2000)
-    gan.save("models/celeba_began_32")
+    gan.train(x, epochs=1000001, k_lambda=0.001, gamma=0.8, batch_size=16, sample_interval=500,
+              sample_path="samples/celeba_began_batchnorm_32", save_interval=5000)
+    gan.save("models/celeba_began_batchnorm_32")
